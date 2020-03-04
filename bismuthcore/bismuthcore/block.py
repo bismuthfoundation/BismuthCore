3f844d1f088de48ad3069b46670eed74377492ee
EggPool <dev@eggpool.net>
2020-03-04 11:38:15 +0100

    Block Object

--- bismuthcore/bismuthcore/block.py
+++ bismuthcore/bismuthcore/block.py
@@ -3,6 +3,8 @@
 Bismuth core Block Class
 """
 
+from bismuthcore.transaction import Transaction
+
 
 class Block:
     """A generic Bismuth Block with its transactions"""
@@ -10,8 +12,17 @@ class Block:
     # Inner storage is compact, binary form
     __slots__ = ('transactions', )
 
-    def __init__(self, transactions):
-        """Default constructor with list of binary, non verbose, transactions, mining transaction at the end."""
+    def __init__(self, transactions: list):
+        """Default constructor with list of binary, non verbose,
+        Transactions instances, mining transaction at the end."""
         self.transactions = transactions
 
-
+    def to_listofdicts(self, legacy: bool=False, decode_pubkey: bool=False) -> list:
+        """
+        The block as a list of Transaction, converted to the required format.
+        :param legacy:
+        :param decode_pubkey:
+        :return:
+        """
+        # Could be better as tuple, but may constraint higher level usage (may need to add something to the list) - to be checked later on
+        return [transaction.to_dict(legacy=legacy, decode_pubkey=decode_pubkey) for transaction in self.transactions]

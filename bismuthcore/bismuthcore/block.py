ad45ee3ccdc3cf04414d422c063ddcbdf38b44d9
EggPool <dev@eggpool.net>
2020-03-04 12:28:41 +0100

    More block method

--- bismuthcore/bismuthcore/block.py
+++ bismuthcore/bismuthcore/block.py
@@ -5,6 +5,8 @@ Bismuth core Block Class
 
 from bismuthcore.transaction import Transaction
 
+__version__ = "0.0.1"
+
 
 class Block:
     """A generic Bismuth Block with its transactions"""
@@ -19,10 +21,17 @@ class Block:
 
     def to_listofdicts(self, legacy: bool=False, decode_pubkey: bool=False) -> list:
         """
-        The block as a list of Transaction, converted to the required format.
+        The block as a list of Transactions, converted to the required dict format.
         :param legacy:
         :param decode_pubkey:
         :return:
         """
         # Could be better as tuple, but may constraint higher level usage (may need to add something to the list) - to be checked later on
         return [transaction.to_dict(legacy=legacy, decode_pubkey=decode_pubkey) for transaction in self.transactions]
+
+    def to_listoftuples(self) -> list:
+        """
+        The block as a list of Transactions, converted to legacy tuples.
+        :return:
+        """
+        return [transaction.to_tuple() for transaction in self.transactions]

753cc7479aaf5ac365d897a822a506cbf6f464b8
EggPool <dev@eggpool.net>
2020-04-04 12:18:24 +0200

    0.0.9

--- bismuthcore/bismuthcore/block.py
+++ bismuthcore/bismuthcore/block.py
@@ -5,7 +5,7 @@ Bismuth core Block Class
 
 from bismuthcore.transaction import Transaction
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 class Block:
@@ -35,3 +35,31 @@ class Block:
         :return:
         """
         return [transaction.to_tuple() for transaction in self.transactions]
+
+    def to_blocks_dict(self) -> dict:
+        """a Block instance can also be a list of transactions from several different blocks.
+        This formatter - ported from ApiHandler - does convert to a dict, heights as key"""
+        # This is a specific format which use and specifics should be documented.
+        tx_list = []
+        block = {}
+        blocks = {}
+        # Egg: mostly kept the previous logic
+        old = None
+        for transaction_object in self.transactions:
+            transaction = transaction_object.to_dict(legacy=True, decode_pubkey=True)
+            height = transaction['block_height']
+            block_hash = transaction['block_hash']
+            del transaction['block_height']
+            del transaction['block_hash']
+            if old != height:  # if same block
+                # using new objects rather than emptying the old ones: same reference, would break if several blocks.
+                tx_list = []
+                block = {}
+            tx_list.append(transaction)
+            block['block_height'] = height
+            block['block_hash'] = block_hash
+            block['transactions'] = tx_list
+            blocks[height] = block
+            old = height  # update
+        return blocks
+

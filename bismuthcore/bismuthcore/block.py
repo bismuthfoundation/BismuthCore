61a1a5070c2d781009d881bb2ab23113a908f99c
Aareon Sullivan <askully13@gmail.com>
2024-03-28 15:16:53 -0500

    Remove unused import

--- bismuthcore/bismuthcore/block.py
+++ bismuthcore/bismuthcore/block.py
@@ -8,9 +8,8 @@ from typing import List
 from bismuthcore.helpers import address_validate, address_is_rsa
 from time import time as ttime
 from polysign.signerfactory import SignerFactory
-from base64 import b64decode, b64encode
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 class Block(TransactionsList):
